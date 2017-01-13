2731349a49e220570f906e6e0ab12d2c3ab4cb56
Aydin Mir Mohammadi <am@bluehands.de>
2017-01-13 14:43:25 +0100

    Initial commit

---
+++ 03 Code/Prototypes/Sandbox/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bluehands.Diagnostics.Log;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new Log<Program>();
+            log.Error("Das sollte nicht passieren");
+        }
+    }
+}
---
+++ 05 Test/Unit Testing/Bluehands.Diagnostics.Log.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bluehands.Diagnostics.Log.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

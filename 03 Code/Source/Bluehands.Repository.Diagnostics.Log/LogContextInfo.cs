4d6e490998ad1b762cf57ab66b7d7688f4478d77
Aydin Mir Mohammadi <am@bluehands.de>
2017-01-13 15:00:57 +0100

    Now all files are present in the commit

---
+++ 03 Code/Source/Bluehands.Repository.Diagnostics.Log/LogContextInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Bluehands.Diagnostics.Log
+{
+    public class LogContextInfo
+    {
+        public string ApplicationContextId { get; set; }
+        public string DatabaseContextId { get; set; }
+        public string DatabaseUserContextId { get; set; }
+        public string UserContextId { get; set; }
+        public string ApplicationName { get; set; }
+        public string DatabaseId { get; set; }
+        public string UserId { get; set; }
+        public string ObfuscatedSessionToken { get; set; }
+    }
+}
---
+++ 03 Code/Source/Bluehands.Repository.Diagnostics.Log/LogLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace Bluehands.Diagnostics.Log
+{
+    public enum LogLevel
+    {
+        Fatal,
+        Error,
+        Warning,
+        Info,
+        Debug,
+        Trace,
+    }
+}

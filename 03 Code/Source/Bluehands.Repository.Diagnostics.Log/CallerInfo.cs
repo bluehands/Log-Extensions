a7e21aaabc09813f168a247da3e38bf0f41fccf5
Laura Ganzer <Ganzer@bluehands.de>
2017-01-27 09:53:41 +0100

    - cleaned up

--- 03 Code/Source/Bluehands.Repository.Diagnostics.Log/CallerInfo.cs
+++ 03 Code/Source/Bluehands.Repository.Diagnostics.Log/CallerInfo.cs
@@ -5,13 +5,18 @@ using System.Text;
 
 namespace Bluehands.Repository.Diagnostics.Log
 {
-    public class CallerInfos
+    public class CallerInfo
     {
         public readonly string NamespaceOfCallerOfLog;
         public readonly string ClassNameOfLog;
         public readonly string MethodNameOfCallerOfLog;
 
-        public CallerInfos(string namespaceOfCallerOfLog, string classNameOfLog, string methodNameOfCallerOfLog)
+        public CallerInfo()
+        {
+
+        }
+
+        public CallerInfo(string namespaceOfCallerOfLog, string classNameOfLog, string methodNameOfCallerOfLog)
         {
             NamespaceOfCallerOfLog = namespaceOfCallerOfLog;
             ClassNameOfLog = classNameOfLog;
--- 03 Code/Source/Bluehands.Repository.Diagnostics.Log/LogMessageWriter.cs
+++ 03 Code/Source/Bluehands.Repository.Diagnostics.Log/LogMessageWriter.cs
@@ -8,21 +8,23 @@ namespace Bluehands.Repository.Diagnostics.Log
         private readonly Logger m_NLogLog;
         //private static Logger nLogLog = LogManager.GetCurrentClassLogger();
         private readonly NLogMessageBuilder m_NLogMessageBuilder;
+        private Type m_CallerOfLog;
 
         public LogMessageWriter(Type callerOfLog, Type callerOfLogMessageWriter)
         {
             m_NLogMessageBuilder = new NLogMessageBuilder(callerOfLogMessageWriter);
             m_NLogLog = LogManager.GetLogger(callerOfLog.FullName);
+            m_CallerOfLog = callerOfLog;
         }
 
         public void WriteLogEntry(LogLevel logLevel, string message)
         {
-            WriteLogEntry(logLevel, message, null);
+            WriteLogEntry(logLevel, message,null);
         }
 
         public void WriteLogEntry(LogLevel logLevel, string message, Exception ex)
         {
-            var logEventInfo = m_NLogMessageBuilder.GetLogEventInfo(logLevel, message, ex);
+            var logEventInfo = m_NLogMessageBuilder.GetLogEventInfo(logLevel, message, m_CallerOfLog, ex);
             m_NLogLog.Log(logEventInfo);
         }
     }

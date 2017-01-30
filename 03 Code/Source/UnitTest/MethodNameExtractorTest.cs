0a3e40fc6dd042a9cdfa96ba6bf0feeffa9d5694
Laura Ganzer <Ganzer@bluehands.de>
2017-01-30 15:12:12 +0100

    -add MethodNameExtractorTest

---
+++ 03 Code/Source/UnitTest/MethodNameExtractorTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Bluehands.Repository.Diagnostics.Log;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    public class SampleCallerOfGroundForExtractor
+    {
+        private readonly MethodNameExtracter m_Extractor;
+
+        public SampleCallerOfGroundForExtractor(MethodNameExtracter extractor)
+        {
+            m_Extractor = extractor;
+        }
+        public CallerInfo DoIt()
+        {
+            return m_Extractor.ExtractCallerInfoFromStackTrace();
+        }
+    }
+
+    [TestClass]
+    public class MethodNameExtractorTest
+    {
+        [TestMethod]
+        public void ExtractWithSampleCall()
+        {
+            //Arrange
+            var sut = new MethodNameExtracter(typeof(SampleCallerOfGroundForExtractor));
+            var sample = new SampleCallerOfGroundForExtractor(sut);
+            //Act
+            var callerInfo = sample.DoIt();
+            //Assert
+            Assert.IsNotNull(callerInfo);
+            Assert.AreEqual(nameof(MethodNameExtractorTest), callerInfo.ClassNameOfGround);
+            Assert.AreEqual(nameof(ExtractWithSampleCall), callerInfo.MethodNameOfCallerOfGround);
+            Assert.AreEqual(typeof(MethodNameExtractorTest).ToString(), callerInfo.TypeOfCallerOfGround);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CheckForNullInCtor()
+        {
+            //Arrange
+            var methodNameExtracter = new MethodNameExtracter(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void CheckFrameIsNotInStackTrace()
+        {
+            //Arrange
+            var sut = new MethodNameExtracter(typeof(NotUsedClass));
+            var sample = new SampleCallerOfGroundForExtractor(sut);
+            //Act
+            sample.DoIt();
+        }
+    }
+}

2af64d7c3b4bf364efee050ce27b19970716962f
Marcel Danz <danz@bluehands.de>
2017-05-18 15:55:25 +0200

    added Install.ps1 file from nlog.config nuget package to copy example Nuget.config to output

---
+++ 03 Code/Source/Bluehands.Repository.Diagnostics.Log/tools/Install.ps1
@@ -0,0 +1,15 @@
+﻿# Copyright (c) 2004-2016 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen
+
+# All rights reserved.
+
+param($installPath, $toolsPath, $package, $project)
+
+$configItem = $project.ProjectItems.Item("NLog.config")
+
+# set 'Copy To Output Directory' to 'Copy if newer'
+$copyToOutput = $configItem.Properties.Item("CopyToOutputDirectory")
+$copyToOutput.Value = 1
+
+# set 'Build Action' to 'Content'
+$buildAction = $configItem.Properties.Item("BuildAction")
+$buildAction.Value = 2

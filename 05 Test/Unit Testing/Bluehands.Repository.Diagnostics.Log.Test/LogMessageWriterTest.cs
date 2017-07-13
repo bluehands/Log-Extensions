4fa71c85ce6493fce594fefe833d9d69ca44f973
Aydin Mir Mohammadi <am@bluehands.de>
2017-07-13 14:53:20 +0200

    Fix Test

--- 05 Test/Unit Testing/Bluehands.Repository.Diagnostics.Log.Test/LogMessageWriterTest.cs
+++ 05 Test/Unit Testing/Bluehands.Repository.Diagnostics.Log.Test/LogMessageWriterTest.cs
@@ -13,19 +13,6 @@ namespace Bluehands.Repository.Diagnostics.Log.Test
 		private readonly LogMessageWriter m_LogMessageWriter = new LogMessageWriter(typeof(LogMessageWriterTest));
 		private static readonly Func<string> TestMessage = () => "Test message.";
 
-		[TestMethod]
-		public void Given_NullMessage_When_WriteLogEntryWithLogLevelDebugAndCallerMethodName_Then_LogStringEmpty()
-		{
-			var writer = new StringWriter();
-			Console.SetOut(writer);
-
-			//When
-			m_LogMessageWriter.WriteLogEntry(LogLevel.Debug, null, System.Reflection.MethodBase.GetCurrentMethod().Name, null);
-			var logString = writer.ToString();
-
-			//Then
-			Assert.IsTrue(logString.Contains("System.ArgumentNullException"));
-		}
 
 		[TestMethod]
 		public void Given_ArgumentNullExceptionArgument_When_WriteLogEntryWithLogLevelErrorAndCallerName_Then_LogStringContainsErrorAndTestMessageAndArgumentNullException()
@@ -43,7 +30,7 @@ namespace Bluehands.Repository.Diagnostics.Log.Test
 			var logColumns = logString.Split('|');
 			Assert.AreEqual(LogLevel.Error.ToString().ToUpper() + ":", logColumns[2].Trim());
 			Assert.AreEqual(TestMessage(), logColumns[6].Trim());
-			Assert.AreEqual(expectedException.ToString(), logColumns[7].TrimEnd());
+			Assert.IsTrue(logColumns[7].Contains(expectedException.ToString()));
 
 		}
 	}

8d971ab211bf0acd5c336a916916513f0e1c7836
Alexander Wiedemann <wiedemann@bluehands.de>
2018-09-21 16:52:30 +0200

    - indent with two spaces again

--- 03 Code/Source/Bluehands.Diagnostics.LogExtensions/NLogMessageBuilder.cs
+++ 03 Code/Source/Bluehands.Diagnostics.LogExtensions/NLogMessageBuilder.cs
@@ -62,25 +62,25 @@ namespace Bluehands.Diagnostics.LogExtensions
                 case 0:
                     return "";
                 case 1:
-                    return " ";
-                case 2:
                     return "  ";
-                case 3:
-                    return "   ";
-                case 4:
+                case 2:
                     return "    ";
-                case 5:
-                    return "     ";
-                case 6:
+                case 3:
                     return "      ";
-                case 7:
-                    return "       ";
-                case 8:
+                case 4:
                     return "        ";
+                case 5:
+                    return "          ";
+                case 6:
+                    return "            ";
+                case 7:
+                    return "              ";
+                case 8:
+                    return "                ";
                 case 9:
-                    return "         ";
+                    return "                  ";
                 default:
-                    return new string(' ', indent);
+                    return new string(' ', indent * 2);
             }
         }
 

11c10b38a16bc9043c21f270d33351b5b7be6df3
Aydin Mir Mohammadi <am@bluehands.de>
2019-04-29 10:57:15 +0200

    Add Logcat target Add Nuspec Update version

---
+++ 03 Code/Source/NLog.Targets.Logcat/Properties/AssemblyVersionInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
